b729cf47aa62a6a5809fb7ba69f1536080697078
مهدی فرهانی <m.farhani@entekhabgroup.ir>
2025-07-19 23:58:13 +0330

    Polynomial multivariate

---
+++ housing-05.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+fileName='House-03.csv'
+dataframe = pd.read_csv(fileName)
+dataframe = dataframe.iloc[1:].reset_index(drop=True)
+
+
+print(dataframe.head())
+
+#print(dataframe)
+
+
+plt.figure (figsize=(8,6))
+plt.scatter(dataframe['M2'],dataframe['Price'],color='blue',alpha=0.5)
+plt.title('Entekhab Housing Price')
+plt.xlabel('Meter')
+plt.ylabel('Price (100 Million Toman)')
+plt.grid(False)
+plt.tight_layout()
+plt.show()
+
